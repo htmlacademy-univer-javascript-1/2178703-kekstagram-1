f5213cb22a332fedfcccb90a81b22e5079bfcdd6
Елена Латынцева <Elena.Latyntseva@urfu.me>
2022-09-13 23:38:54 +0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let getRandomInt = function(leftBorder, rightBorder) {
+  if(leftBorder > rightBorder){
+  throw new Error('Левая граница больше правой');
+  }
+
+  return Math.floor(Math.random() * (rightBorder + 1 - leftBorder)) + leftBorder;
+}
+
+  let checkLengthOfComment = (comment, maxLength) => String(comment).length <= maxLength;
