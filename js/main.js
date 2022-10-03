f49d5b7ee1ef42aeb7dda206d07d772261e6e139
Елена Латынцева <Elena.Latyntseva@urfu.me>
2022-10-03 11:09:07 +0500

    Модуляция

--- js/main.js
+++ js/main.js
@@ -36,10 +36,6 @@ const getRandomPositiveInteger = (a, b) => {
 
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-const createPhotoArray = (lengthArray) => Array.from({length: lengthArray}).map((value, index) => createPhotoData(index + 1));
-
-
-
 const createComment = (id) => ({
   id,
   avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
@@ -55,8 +51,10 @@ const createPhotoData = (id) => ({
   comments: Array.from({length: getRandomPositiveInteger(1, COUNT_COMMENT)}).map((value, index) => createComment(index + 1))
 });
 
+const createPhotoArray = (lengthArray) => Array.from({length: lengthArray}).map((value, index) => createPhotoData(index + 1));
+
 const photos = createPhotoArray(COUNT_PHOTO);
 
 checkStringLength('Вызов функции', 140);
-console.log(photos[6].comments[1]);
+photos();
 
