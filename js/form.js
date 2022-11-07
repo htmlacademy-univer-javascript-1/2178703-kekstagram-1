833bafcfbd5b01feb7c6a81fb183752f74cfbc12
Елена Латынцева <Elena.Latyntseva@urfu.me>
2022-11-07 17:39:07 +0500

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -1,19 +1,29 @@
 import { isEscapeKey, checkStringLength } from './utils.js';
-import { MAX_STRING_LENGTH, MAX_COUNT_HASHTAGS } from './consts.js';
+import { MAX_STRING_LENGTH, MAX_HASHTAG_COUNT, MAX_HASHTAG_LENGTH } from './consts.js';
 
+const body = document.querySelector('body');
 const imgUploadField = document.querySelector('#upload-file');
 const editImg = document.querySelector('.img-upload__overlay');
 const form = document.querySelector('.img-upload__form');
-const closeButton = document.querySelector('.img-upload__cancel');
-const hashtagsField = document.querySelector('.text__hashtags');
-const commentsField = document.querySelector('.text__description');
-const regex = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const closeButton = form.querySelector('.img-upload__cancel');
+const hashtagsField = form.querySelector('.text__hashtags');
+const commentsField = form.querySelector('.text__description');
+
+
+const pristine = new Pristine(form, {
+  classTo: 'text',
+  errorClass: 'text--invalid',
+  successClass: 'text-valid',
+  errorTextParent: 'text',
+  errorTextTag: 'div',
+  errorTextClass: 'text__error'
+});
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     editImg.classList.add('hidden');
-    document.body.classList.remove('modal-open');
+    body.classList.remove('modal-open');
     document.removeEventListener('keydown', onPopupEscKeydown);
     form.reset();
   }
@@ -21,7 +31,7 @@ const onPopupEscKeydown = (evt) => {
 
 const closeUploadPopup  = () => {
   editImg.classList.add('hidden');
-  document.body.classList.remove('modal-open');
+  body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
   document.removeEventListener('click', closeUploadPopup);
   form.reset();
@@ -38,60 +48,114 @@ const addFieldListeners = (field) => {
 
 const showUploadPopup = () => {
   editImg.classList.remove('hidden');
-  document.body.classList.add('modal-open');
+  body.classList.add('modal-open');
   closeButton.addEventListener('click', closeUploadPopup);
   document.addEventListener('keydown',onPopupEscKeydown);
   addFieldListeners(commentsField);
   addFieldListeners(hashtagsField);
 };
 
-const validateCommentLength = (string) => checkStringLength(string,  MAX_STRING_LENGTH);
-
-const getHashtags = (string) => string.split(' ').filter((item) => item !== '');
-
-const getUniqueHashtags = (string) => {
-  const hashtags = getHashtags(string);
+const getUniqueHashtags = (hashtags) => {
   const uniqueSet = new Set(hashtags);
   return hashtags.length === uniqueSet.size;
 };
 
-const checkQuantity = (string) => checkStringLength(getHashtags(string), MAX_COUNT_HASHTAGS);
+let errorMessage = '';
 
-const getHashtagsToLowerCase = (string) => {
-  const hashtags = getHashtags(string);
-  return hashtags.map((element) => element.toLowerCase());
+const error = () => errorMessage;
+
+const hashtagsHandler = (string) => {
+  errorMessage = '';
+
+  const inputText = string.toLowerCase().trim();
+
+  if(!inputText) {
+    return true;
+  }
+
+  const inputHashtags = inputText.split(/\s+/);
+
+  if(inputHashtags.length === 0) {
+    return true;
+  }
+
+  const rules = [
+    {
+      check: inputHashtags.some((item) => item.indexOf('#', 1) >= 1),
+      error: 'Хэш-теги должны разделяться пробелами',
+    },
+
+    {
+      check: inputHashtags.length > MAX_HASHTAG_COUNT,
+      error: `Нельзя указать больше ${MAX_HASHTAG_COUNT} хэш-тегов`,
+    },
+
+    {
+      check: inputHashtags.some((item) => item[0] !== '#'),
+      error: 'Хэш-тег должен начинаться с символа #',
+    },
+
+    {
+      check: inputHashtags.some((item) => item.length > MAX_HASHTAG_LENGTH),
+      error: `Максимальная длина одного хэш-тега ${MAX_HASHTAG_LENGTH} символов, включая #`,
+    },
+
+    {
+      check: inputHashtags.some((item) => !/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/.test(item)),
+      error: 'Хэш-тег должен содержать только буквы и цифры',
+    },
+
+    {
+      check: !getUniqueHashtags(inputHashtags),
+      error: 'Хэш-теги не должны повторяться',
+    },
+  ];
+
+  return rules.every((rule) => {
+    const isInvalid = rule.check;
+    if(isInvalid) {
+      errorMessage = rule.error;
+    }
+    return !isInvalid;
+  });
 };
 
-const checkHashtagsSymbols = (string) => {
-  const hashtags = getHashtags(string);
-  return hashtags.every((element) => regex.test(element));
+const commentHandler = (string) => {
+  errorMessage = '';
+
+  const inputText = string.trim();
+
+  if(!inputText) {
+    return true;
+  }
+
+  const rule = {
+      check: !checkStringLength(inputText, MAX_STRING_LENGTH),
+      error: `Максимальная длина комментария ${MAX_STRING_LENGTH} символов`,
+    };
+
+
+  const isInvalid = rule.check;
+  if(isInvalid) {
+    errorMessage = rule.error;
+  }
+  return !isInvalid;
 };
 
 const validateForm = () => {
-  const pristine = new Pristine(form, {
-    classTo: 'text',
-    errorClass: 'text--invalid',
-    successClass: 'text-valid',
-    errorTextParent: 'text',
-    errorTextTag: 'div',
-    errorTextClass: 'text__error'
-  });
+  pristine.addValidator(hashtagsField, hashtagsHandler, error);
+  pristine.addValidator(commentsField, commentHandler, error);
+};
 
-  pristine.addValidator(commentsField, validateCommentLength, `Не более ${MAX_STRING_LENGTH} символов`);
-  pristine.addValidator(hashtagsField, getUniqueHashtags, 'Хэштеги не могут повторяться');
-  pristine.addValidator(hashtagsField, checkQuantity, `Не более ${MAX_COUNT_HASHTAGS} хэштегов`);
-  pristine.addValidator(hashtagsField, getHashtagsToLowerCase, '');
-  pristine.addValidator(hashtagsField, checkHashtagsSymbols, 'Хэштег должен начинатьтся с #, содержать только буквы и цифры, не более 20 символов');
+
+const renderUploadForm = () => {
+  imgUploadField.addEventListener('change', showUploadPopup);
+  validateForm();
   form.addEventListener('submit', (evt) => {
     if (!pristine.validate()) {
       evt.preventDefault();
     }
   });
-};
-
-const renderUploadForm = () => {
-  imgUploadField.addEventListener('change', showUploadPopup);
-  validateForm();
 };
 
 export {renderUploadForm};
