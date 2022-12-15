da200d44b4932139676e0f51c1540c2153b09c5c
Елена Латынцева <Elena.Latyntseva@urfu.me>
2022-12-15 19:13:56 +0500

    Фото на память

--- js/user-photo.js
+++ js/user-photo.js
@@ -8,16 +8,14 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
     const pictureUrl = URL.createObjectURL(file);
     imgPreview.src = pictureUrl;
     effectPreviews.forEach((effect) => {
-        effect.style.backgroundImage = `url(${pictureUrl})`;
-      });
+      effect.style.backgroundImage = `url(${pictureUrl})`;
+    });
   }
 });
 
